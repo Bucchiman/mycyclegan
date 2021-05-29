e75edb888d469b0b5f1d3c8bec3500faf66eea82
Yuki Iwabuchi <iwabuchi@v102.fun.bio.keio.ac.jp>
2021-05-29 20:26:40 +0900

    script change

--- scripts/run_gpu.sh
+++ scripts/run_gpu.sh
@@ -2,14 +2,13 @@
 #
 # FileName: 	run_gpu
 # CreatedDate:  2021-05-26 02:22:41 +0900
-# LastModified: 2021-05-28 20:57:57 +0900
+# LastModified: 2021-05-29 03:52:18 +0900
 #
 
 
-#nohup python main.py ../datas ../outputs --device cuda:1 --dataset_name mycustom --n_epochs 100 --decay_epoch 80 --sample_interval 50 --checkpoint_interval 40 > hoge.log &
+nohup python main.py ../datas ../outputs --device cuda:0 --dataset_name mycustom --n_epochs 100 --decay_epoch 80 --sample_interval 20 --checkpoint_interval 10 --batch_size 1 > results.log &
 
-cd ../src
-export CUDA_VISIBLE_DEVICES=2,3
-nohup python main.py ../datas ../outputs --device cuda --multigpu --dataset_name mycustom --n_epochs 100 --decay_epoch 80 --sample_interval 20 --checkpoint_interval 20 --batch_size 2 > results.log &
-
+#export CUDA_VISIBLE_DEVICES=2,3
+#nohup python main.py ../datas ../outputs --device cuda --multigpu --dataset_name mycustom --n_epochs 100 --decay_epoch 80 --sample_interval 20 --checkpoint_interval 20 --batch_size 2 > results.log &
+#
 return
