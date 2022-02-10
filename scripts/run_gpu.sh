7d3298f45e309cd3da02861abe144f490221a5c0
Yuki Iwabuchi <iwabuchi@ac922-3.q.st.keio.ac.jp>
2022-02-11 05:38:20 +0900

    multigpu method added

--- scripts/run_gpu.sh
+++ scripts/run_gpu.sh
@@ -11,4 +11,9 @@ nohup python main.py ../datas ../outputs --device cuda:0 --dataset_name mycustom
 #export CUDA_VISIBLE_DEVICES=2,3
 #nohup python main.py ../datas ../outputs --device cuda --multigpu --dataset_name mycustom --n_epochs 100 --decay_epoch 80 --sample_interval 20 --checkpoint_interval 20 --batch_size 2 > results.log &
 #
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+nohup python main.py ../datas ../outputs --device cuda --multigpu --initial --dataset_name mycustom --n_epochs 100 --decay_epoch 80 --sample_interval 100 --checkpoint_interval 20 --batch_size 2 > results.log &
+
+
 return
